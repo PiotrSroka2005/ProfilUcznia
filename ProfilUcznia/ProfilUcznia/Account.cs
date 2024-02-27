6cf4cbd67d1fb263e52ef6dd9c49fa8e7c45a750
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-27 13:11:25 +0100

    Add Account class

---
+++ ProfilUcznia/ProfilUcznia/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProfilUcznia
+{
+    public class Account
+    {
+        public Account(string login, string password, UserClass @class)
+        {
+            Login = login;
+            Password = password;
+            Class = @class;
+        }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public UserClass Class { get; set; }
+    }
+}
