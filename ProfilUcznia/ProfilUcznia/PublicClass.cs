302afecdaf732a03d3e94e172c62760c5e3279b2
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-27 13:12:27 +0100

    Add PublicClass

---
+++ ProfilUcznia/ProfilUcznia/PublicClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProfilUcznia
+{
+    static public class PublicClass
+    {
+        static public List<Account> accounts = new List<Account>();
+        static public List<UserClass> Clases = new List<UserClass>();
+    }
+}
