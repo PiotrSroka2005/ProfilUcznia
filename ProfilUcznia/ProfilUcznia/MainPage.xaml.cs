5e36de50aac812cb52e4fe1eed26f29d29baa6e8
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-27 14:10:34 +0100

    Add links to PublicClass for exact class

--- ProfilUcznia/ProfilUcznia/MainPage.xaml.cs
+++ ProfilUcznia/ProfilUcznia/MainPage.xaml.cs
@@ -13,6 +13,10 @@ namespace ProfilUcznia
         public MainPage()
         {
             InitializeComponent();
+            PublicClass.Clases.Add(new UserClass("4TP", "https://zstio.edu.pl/plan/u/podzgodz/plan.php?kl=123F69DA832927A6"));
+            PublicClass.Clases.Add(new UserClass("3TP", "https://zstio.edu.pl/plan/u/podzgodz/plan.php?kl=7C176FB779BD7149"));
+            PublicClass.Clases.Add(new UserClass("5TP", "https://zstio.edu.pl/plan/u/podzgodz/plan.php?kl=2FE6DBDB64885790"));
+            PublicClass.accounts.Add(new Account("Piotr", "1234", PublicClass.Clases[0]));            
         }
 
         private void Register(object sender, EventArgs e)
