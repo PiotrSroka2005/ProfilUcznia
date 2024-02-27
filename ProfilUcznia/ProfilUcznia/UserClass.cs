090f6c97c1ce56923a0d8247623bdf2db2eed16b
Uczen <uczen@email.pl>
2024-02-27 13:08:14 +0100

    Add UserClass

---
+++ ProfilUcznia/ProfilUcznia/UserClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProfilUcznia
+{
+    public class UserClass
+    {
+        public UserClass(string @class, string path)
+        {
+            Class = @class;
+            Path = path;
+        }
+        public UserClass()
+        {
+
+        }
+
+        public string Class { get; set; }
+        public string Path { get; set; }
+    }
+}
